3f6e0ae57934b5e7a899b4ee6be39dfab0a243e2
nallamsrikanth2 <nallamsrikanth12>
2026-04-02 23:54:12 +0530

    first commit

--- 04-conf.sh
+++ 04-conf.sh
@@ -5,7 +5,4 @@
  echo "enter the password"
  read -s PASSWORD
 
- echo "my username is $USERNAME and my password is $PASSWORD"
-
-
- echo -p "my username $USERNAME AND PASSWORD IS $PASSWORD"
+ echo "my username is $USERNAME and my password is $PASSWd
