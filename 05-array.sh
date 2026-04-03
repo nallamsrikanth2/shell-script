2b3617a167b89fad7ae9bf64ebae41a1be31e65b
nallamsrikanth2 <nallamsrikanth12>
2026-04-03 19:49:12 +0530

    array

--- 05-array.sh
+++ 05-array.sh
@@ -5,7 +5,5 @@ MOVIES=("RRR" "DJtillu" "og" "couple friendly")
 echo "first movie is ${MOVIES[0]}"
 echo "second movie is ${MOVIES[1]}"
 echo "third movie is ${MOVIES[2]}"
-
-
-echo "first movie is : ${MOVIES[3]}"
+echo "fourth movie is : ${MOVIES[3]}"
 
