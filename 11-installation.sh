4b93da671350aa9eff643a876b4cf72719c843e5
nallamsrikanth2 <nallamsrikanth12>
2026-04-05 23:57:02 +0530

    shell script

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-NAME=$1
+USERID=(id -u)
 
-echo "my name $NAME"
+if [$USERID -ne 0]
+then
+    echo "please run with code in super user"
+    exit 1
+else
+    echo "you are a super user"
+fi
 
+
+dnf install mysql -y
+
+if [$? -ne 0]
+then
+    echo "my mysql is..failure"
+    exit 1
+else
+    echo "mysql is sucess"
+fi
+
+
+dnf install git -y
+
+echo "This is basic shell-scripting"
