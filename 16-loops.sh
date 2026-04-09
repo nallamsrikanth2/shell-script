51b7256c0680c1013e4e9b06816920bde19573c8
nallamsrikanth2 <nallamsrikanth12>
2026-04-09 20:28:04 +0530

    functions

---
+++ 16-loops.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOF_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+if [ $USERID -ne 0]
+then 
+    echo "please run the script inside the root server"
+else
+    echo "you are in root user"
+fi
+
+for i in "$@"
+do
+    echo "packages to install $i"
+    dnf list installed $i  &>>$SCRIPT_NAME
+    if [ $? -eq 0 ]
+    then
+        echo "alreay installed ....SKIPING"
+    else
+        echo "need to install"
+    fi
+done
+
