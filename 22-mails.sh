7930801406ccb10e477543a45a710044c4147205
venkateshGadiparthi <venkateshchowdary7171@gmail.com>
2026-04-17 19:58:53 +0530

    mails

--- 22-mails.sh
+++ 22-mails.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+DISK_USAGE=$(df -h | grep xfs)
 THRESHOLD=10
+MESSAGE=""
+
+
+while IFS read -r line
+do
+USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+FOLDER=$( echo $line | awk -F " " '{print $NF}')
+if [ $USAGE -ge $THRESHOLD]
+then
+    MESSAGE+="$FOLDER is more than $THRESHOLD, curren usage : $USAGE"
+fi
+done <<< $DISK_USAGE
